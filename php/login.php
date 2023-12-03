0b6f974dbbf864d2a4b85f0a05910a13b4cae285
MR187078 <139504344+MR187078@users.noreply.github.com>
2023-12-03 16:18:48 -0600

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,5 +1,4 @@
 <?php
-
 // Inicia la sesión
 session_start();
 
@@ -8,33 +7,42 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "realty";
 
-
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-if (!$conn) 
-{
-	die("No hay conexión: ".mysqli_connect_error());
+if (!$conn) {
+	die("No hay conexión: " . mysqli_connect_error());
 }
 
 $nombre = $_POST["txtusuario"];
 $pass = $_POST["txtpassword"];
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios WHERE correo = '$nombre' and contrasena = '$pass'");
+$query = mysqli_query($conn, "SELECT id_usuario FROM usuarios WHERE correo = '$nombre' AND contrasena = '$pass'");
 $nr = mysqli_num_rows($query);
 
-if($nr == 1)
-{
-    $_SESSION["txtusuario"] = $nombre;
-    $_SESSION["txtpassword"] = $pass;
-	header("Location: ../menu-control.php");
+if ($nr == 1) {
+	$row = mysqli_fetch_assoc($query);
+	$_SESSION["txtusuario"] = $nombre;
+	$_SESSION["txtpassword"] = $pass;
+	$_SESSION['id_usuario'] = $row['id_usuario'];
+	
+	$propiedades_query = mysqli_query($conn, "SELECT * FROM propiedades WHERE id_usuario = {$_SESSION['id_usuario']}");
+    $propiedades = array();
+
+    // Almacena las propiedades en un array asociativo
+    while ($propiedad = mysqli_fetch_assoc($propiedades_query)) {
+        $propiedades[] = $propiedad;
+    }
+   
+	// Almacena el array de propiedades en la sesión
+    $_SESSION['propiedades'] = $propiedades;
+
+    header("Location: ../menu-control.php");
     exit;
-}
-else if ($nr == 0) 
-{
-    header("Location: ../login.php");
-    exit;
+
+} else {
+	header("Location: ../login.php");
+	exit;
 }
 
 $conn->close();
-
 ?>
